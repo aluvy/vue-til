f8c6915e049fa355f167c11e340a5433ee8345a8
songmiok <aluvy_@naver.com>
2024-02-08 12:55:52 +0900

    Update App.vue

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,13 +1,29 @@
 <template>
-  <div></div>
+	<div>
+		<header>
+			<router-link to="/login">login</router-link>
+			<router-link to="/signup">signup</router-link>
+		</header>
+		<transition name="pageChange">
+			<router-view></router-view>
+		</transition>
+	</div>
 </template>
 
 <script>
-export default {
 
-}
+export default {
+};
 </script>
 
-<style>
+<style scoped>
+.pageChange-enter-active,
+.pageChange-leave-active { animation: pageChange .2s; }
+.pageChange-enter-active { animation-delay: .1s; animation-fill-mode: backwards; }
+.pageChange-leave-active { position: absolute; animation-direction: reverse; }
 
+@keyframes pageChange {
+	0% { opacity: 0; }
+	100% { opacity: 1; }
+}
 </style>
