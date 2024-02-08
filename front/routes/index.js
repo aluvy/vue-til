4b2c53046075570fb64ac4e727872f486a1498d9
songmiok <aluvy_@naver.com>
2024-02-08 12:55:38 +0900

    Create index.js

---
+++ front/routes/index.js
@@ -0,0 +1,31 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import LoginPage from '@/views/LoginPage.vue';
+
+Vue.use(VueRouter);
+
+export default new VueRouter({
+	// mode: 'history',
+	// base: '/#',
+	routes: [
+		{
+			path: '/',
+			redirect: '/login',
+		},
+		{
+			path: '/login',
+			name: 'LoginPage',
+			component: LoginPage,
+		},
+		{
+			path: '/signup',
+			name: 'SignupPage',
+			component: () => import('@/views/SignupPage.vue'),
+		},
+		{
+			path: '*',
+			name: 'NotFoundPage',
+			component: () => import('@/views/NotFoundPage.vue'),
+		},
+	],
+});
