975c9d97db5b80ce97a25fe46bb597df4939ff8c
songmiok <aluvy_@naver.com>
2024-02-13 13:18:32 +0900

    Update LoginForm.spec.js

--- front/src/components/LoginForm.spec.js
+++ front/src/components/LoginForm.spec.js
@@ -1,1 +1,11 @@
+import { sum } from './math.js';
 
+// test code
+describe('math.js', () => {
+	test('10 + 20 = 30', () => {
+		const result = sum(10, 20);
+		expect(result).toBe(30);
+	});
+});
+
+// npm t
