a96ab291306420f957fcc783393d93109e14aa89
songmiok <aluvy_@naver.com>
2024-02-13 14:06:42 +0900

    Update LoginForm.spec.js

--- front/src/components/LoginForm.spec.js
+++ front/src/components/LoginForm.spec.js
@@ -1,10 +1,52 @@
-import Vue from 'vue';
+import { shallowMount } from '@vue/test-utils';
+// import Vue from 'vue';
 import LoginForm from './LoginForm.vue';
 
+// test (1)
+// describe('LoginForm.vue', () => {
+// 	test('컴포넌트가 마운팅 되면 username이 존재하고 초기 값으로 빈문자열이 설정되어 있어야 한다.', () => {
+// 		const instance = new Vue(LoginForm).$mount(); // vue에 loginform을 마운트 시킨다
+// 		// console.log(instance);
+// 		expect(instance.username).toBe('');
+// 	});
+// });
+
+// @vue/test-utils 라이브러리 이용
 describe('LoginForm.vue', () => {
-	test('컴포넌트가 마운팅 되면 username이 존재하고 초기 값으로 빈문자열이 설정되어 있어야 한다.', () => {
-		const instance = new Vue(LoginForm).$mount(); // vue에 loginform을 마운트 시킨다
-		// console.log(instance);
-		expect(instance.username).toBe('');
+	// test (2)
+	// test('컴포넌트가 마운팅 되면 username이 존재하고 초기 값으로 빈문자열이 설정되어 있어야 한다.', () => {
+	// 	const wrapper = shallowMount(LoginForm);
+	// 	// wrapper.vm : instance
+	// 	expect(wrapper.vm.username).toBe('');
+	// });
+
+	// test (3)
+	// test('ID는 이메일 형식이어야 한다', () => {
+	// 	const wrapper = shallowMount(LoginForm, {
+	// 		data() {
+	// 			return {
+	// 				username: 'test',
+	// 			};
+	// 		},
+	// 	});
+	// 	const usernameInput = wrapper.findAllComponents(FormInput);
+	// 	console.log('인풋 박스의 값', usernameInput.at(0).props().value); // 결과 : test
+	// 	console.log('isUsernameValid 의 값', wrapper.vm.isUsernameValid); // false
+	// });
+
+	// test (4)
+	// 사용자 관점믜 테스트 코드
+	test('ID가 이메일 형식이 아니면 경고 메시지가 출력된다', () => {
+		const wrapper = shallowMount(LoginForm, {
+			data() {
+				return {
+					username: 'test',
+				};
+			},
+		});
+
+		const warningText = wrapper.find('.form-valid.danger');
+		// console.log(warningText.html());
+		expect(warningText.exists()).toBeTruthy(); // exists() 존재하는지 확인, 그 값이 true 인지
 	});
 });
